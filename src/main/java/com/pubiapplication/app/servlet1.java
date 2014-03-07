7e5be0b6288468d5269de15ef058338ca2865e16
Mikroplu <mikroplu@ut.ee>
2014-03-07 13:54:27 +0200

    grht

--- src/main/java/com/pubiapplication/app/servlet1.java
+++ src/main/java/com/pubiapplication/app/servlet1.java
@@ -1,8 +1,5 @@
 package com.pubiapplication.app;
 import javax.servlet.*;
-
-
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -19,6 +16,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
+
+
 @WebServlet(value="/servlet")
 public class servlet1 extends HttpServlet {
 
