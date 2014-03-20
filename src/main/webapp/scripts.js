8625e20d009c5b079b25ce7dd5ce8e5bddb9ead1
Mikroplu <mikroplu@ut.ee>
2014-03-20 19:02:59 +0200

    ghtj

--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 			if (responseJson != null) {
 				var $combobox = $("#linnad");
 				$.each(responseJson, function(key, value) {
-					$('<option>').val(key).text(value).appendTo($combobox);
+					alert(key + "=" + value);
 				});
 			}
 		});
