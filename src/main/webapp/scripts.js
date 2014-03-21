e055d14d2f475cfd54d4cfc11530641743134c58
Mikroplu <mikroplu@ut.ee>
2014-03-21 08:03:37 +0200

    v

--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -1,63 +1,59 @@
 //Peidab kõik elemendid klassiga "content"
 
 $(document).ready(function() {
-	$("#linnad").click(function(event) {
-		$.get('pubid', function(responseJson) {
-			if (responseJson != null) {
-				var $combobox = $("#linnad");
-				$.each(responseJson, function(key, value) {
-					$("#linnad").append('<option>'+value.asukoht+'</option>');
-						
-				});
-			}
+			$("#linnad").click(function(event) {
+						$.get('pubid', function(responseJson) {
+							if (responseJson != null) {
+								var $combobox = $("#linnad");
+								$.each(responseJson, function(key, value) {
+									$("#linnad").append(
+											'<option>' + value.asukoht
+													+ '</option>');
+
+								});
+							}
+						});
+					});
+
+			
+
+			$('#Showcontent1').click(function() {
+				$('.contents').hide();
+				$('#content1').show();
+			});
+			$('#Showcontent2').click(function() {
+				$('.contents').hide();
+				$('#content2').show();
+			});
+			$('#Showcontent3').click(function() {
+				$('.contents').hide();
+				$('#content3').show();
+			});
+			$('#Showcontent4').click(function() {
+				$('.contents').hide();
+				$('#content4').show();
+			});
+
+			// Edasi nupud
+			$('#linna_valik').click(function() {
+				$('.contents').hide();
+				$('#content2').show();
+			});
+
+			$('#pubi_valik').click(function() {
+				$('.contents').hide();
+				$('#content3').show();
+			});
+
+			$('#laua_valik').click(function() {
+				$('.contents').hide();
+				$('#content3').show();
+			});
+
+			// Muudab nuppude klassid
+			$('button').click(function() {
+				$('.menu_button').removeClass('menu_button_clicked');
+				$(this).addClass('menu_button_clicked');
+			});
+
 		});
-	});
-	
-	
-	$("#linnad").on('change',function(){
-		for(var i=0;i<responseJson.length;i++){
-		}
-		
-		
-	});
-
-	$('#Showcontent1').click(function() {
-		$('.contents').hide();
-		$('#content1').show();
-	});
-	$('#Showcontent2').click(function() {
-		$('.contents').hide();
-		$('#content2').show();
-	});
-	$('#Showcontent3').click(function() {
-		$('.contents').hide();
-		$('#content3').show();
-	});
-	$('#Showcontent4').click(function() {
-		$('.contents').hide();
-		$('#content4').show();
-	});
-
-	// Edasi nupud
-	$('#linna_valik').click(function() {
-		$('.contents').hide();
-		$('#content2').show();
-	});
-
-	$('#pubi_valik').click(function() {
-		$('.contents').hide();
-		$('#content3').show();
-	});
-
-	$('#laua_valik').click(function() {
-		$('.contents').hide();
-		$('#content3').show();
-	});
-
-	// Muudab nuppude klassid
-	$('button').click(function() {
-		$('.menu_button').removeClass('menu_button_clicked');
-		$(this).addClass('menu_button_clicked');
-	});
-
-});
