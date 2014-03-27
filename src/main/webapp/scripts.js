a3cb95c5c621f32242c721c33559b4fbe12fe664
mihkelkaasik <mihkelk@ut.ee>
2014-03-27 12:14:47 +0200

    d

--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -20,8 +20,6 @@ $(document).ready (function() {
 	$("#linna_valik").click (function(event) {
 				var linnad = document.getElementById("linnad");
 				var valitud_linn = linnad.options[linnad.selectedIndex].text;
-				alert(valitud_linn);
-				a();
 				$.get('pubinimed', {valitud_linn : valitud_linn}, function(responseJson) {
 					if (responseJson != null) {
 						$.each(responseJson, function(key, value) {
