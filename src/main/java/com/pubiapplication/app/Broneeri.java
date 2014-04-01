fb21ca8d12bf4c8774d9cdea4a8f28b337de6cc8
Mikroplu <mikroplu@ut.ee>
2014-04-01 14:14:05 +0300

    kj

--- src/main/java/com/pubiapplication/app/Broneeri.java
+++ src/main/java/com/pubiapplication/app/Broneeri.java
@@ -43,8 +43,8 @@ public class Broneeri extends HttpServlet {
 		try {
 			String query = "SELECT * FROM Illegard WHERE laua_number=?";
 			PreparedStatement prepStmt2 = conn.prepareStatement(query);
-			prepStmt2.setString(1,pubi_nimi);
-			prepStmt2.setString(2,laua_number);
+			//prepStmt2.setString(1,pubi_nimi);
+			prepStmt2.setString(1,laua_number);
 			ResultSet rs= prepStmt2.executeQuery();
 			if(rs.next()){
 				response(response, "Selline laud on juba broneeritud");
