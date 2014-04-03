3b8bc005ffa9e03a5367d4b1120805bbbce0763d
Mikroplu <mikroplu@ut.ee>
2014-04-04 00:02:42 +0300

    aefae

--- src/main/webapp/facebooklogin.js
+++ src/main/webapp/facebooklogin.js
@@ -49,14 +49,9 @@ function getUserInfo() {
 			.api(
 					'/me',
 					function(response) {
-
 						var str = "<b>Name</b> : " + response.name + "<br>";
-						str += "<b>Link: </b>" + response.link + "<br>";
 						str += "<b>Username:</b> " + response.username + "<br>";
 						str += "<b>id: </b>" + response.id + "<br>";
-						str += "<b>Email:</b> " + response.email + "<br>";
-						str += "<input type='button' value='Get Photo' onclick='getPhoto();'/>";
-						str += "<input type='button' value='Logout' onclick='Logout();'/>";
 						document.getElementById("status").innerHTML = str;
 
 					});
