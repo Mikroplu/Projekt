09414c8a7e37733146ac29f8dc9becd740ba729b
Mikroplu <mikroplu@ut.ee>
2014-04-03 12:48:26 +0300

    er

--- src/main/java/com/pubiapplication/app/Datapush.java
+++ src/main/java/com/pubiapplication/app/Datapush.java
@@ -1,6 +1,10 @@
 package com.pubiapplication.app;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import javax.sql.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/datapush")
 public class Datapush extends HttpServlet {
      
- 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-     
+    	
+    	ArrayList<Pubi> pubid = new ArrayList<Pubi>();
+		pubid = fetchPubid.getAllLinnad();
+
         //content type must be set to text/event-stream
         response.setContentType("text/event-stream");  
  
@@ -21,9 +27,9 @@ public class Datapush extends HttpServlet {
  
         PrintWriter writer = response.getWriter();
  
-        for(int i=0; i<10; i++) {
- 
-            writer.write("data: "+ System.currentTimeMillis() +"\n\n");
+        for(int i=0; i<pubid.size(); i++) {
+        	
+            writer.write("data: "+ (pubid.get(i)).getAsukoht() +"\n\n");
  
             try {
                 Thread.sleep(1000);
